f1c77d83b997e0e1a0a9965631ee5ea4c440b60b
Nick Molargik <nmolargik@gmail.com>
2021-05-05 20:39:42 -0400

    Initial Commit

---
+++ Shared/DriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveApp.swift
+//  Shared
+//
+//  Created by Nick Molargik on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct DriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
